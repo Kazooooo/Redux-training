d6a58d50c6a7e1468fe29c9f3b0ffa1fb2dc531c
KazumasaToda <toda@n-di.co.jp>
2017-04-18 20:03:35 +0900

    enable redux dev-tool

--- src/js/entry.js
+++ src/js/entry.js
@@ -2,7 +2,12 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import { INCREMENT } from './actions/types';
 
-const store = createStore(reducers);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 
 const render = () => {
   document.body.innerText = store.getState().counter;
